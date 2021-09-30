ea20359f5c0902984ceee80d5a8c7e7562fb4bf2
Florent <grenaille.florent85@gmail.com>
2021-09-30 15:03:36 +0200

    Init project with model, view, template and static files

--- ticket/models.py
+++ ticket/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+
+class Intervention(models.Model):
+
+    STATUS_STATE = [
+        ('Brouillon', 'Brouillon'),
+        ('Validé', 'Validé'),
+        ('Terminé', 'Terminé')
+    ]
+
+    label = models.CharField(
+        max_length=250,
+        verbose_name="libellé de l'intervention"
+    )
+    description = models.TextField(
+        verbose_name="description de l'intervention"
+    )
+    agent_name = models.CharField(
+        max_length=120,
+        verbose_name="nom de l'intervenant"
+    )
+    city = models.CharField(
+        max_length=350,
+        verbose_name="lieu de l'intervention"
+    )
+    create_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="intervention créée le"
+    )
+    date = models.DateField(
+        verbose_name="date de l'intervention"
+    )
+    status = models.CharField(
+        max_length=25,
+        choices=STATUS_STATE,
+        default=STATUS_STATE[0][0],
+        verbose_name="statu de l'intervention"
+    )
+    edit = models.BooleanField(
+        default=True,
+        verbose_name="modification de l'intervention"
+    )
--- ticket/views.py
+++ ticket/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.generic.base import TemplateView ,View
+from django.views.generic import ListView
+
+from .models import Intervention
+
+
+class ListingInterventionView(ListView):
+
+    model = Intervention
+    queryset = Intervention.objects.all().order_by('create_at')
+    template_name = "ticket/ticket.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        return context
